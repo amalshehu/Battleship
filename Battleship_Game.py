65d1c30530892a7e756215939a4dee6e2ec625f2
Amal Shehu <amalshehu@gmail.com>
2016-08-29 17:40:13 +0530

    A simple classic game based on Python

---
+++ Battleship_Game.py
@@ -0,0 +1,5 @@
+#  File:       Battleship_Game.py
+#  Purpose:    Game :Simplified version of the classic board game Battleship!
+#  Programmer: Amal Shehu
+#  Course:     Codecademy
+#  Date:       Monday 29th August 2016, 05:30 PM
