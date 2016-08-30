766866bb3669502f23f5d2f7b54f6f10e7222516
Amal Shehu <amalshehu@gmail.com>
2016-08-30 10:11:45 +0530

    Reformated under turn loop

--- Battleship_Game.py
+++ Battleship_Game.py
@@ -13,7 +13,7 @@ for i in range(0,5):
 def print_board(board):
     for item in board:
         print ("  ".join(item))
-
+print ("Let's play Battleship!")
 print_board(board)
 
 def random_row(board):
@@ -24,21 +24,29 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
+for turn in range(4):
+    guess_row = int(input("Guess Row:"))
+    guess_col = int(input("Guess Col: "))
 
-guess_row = int(input("Guess Row:"))
-guess_col = int(input("Guess Col: "))
+    print (ship_col)
+    print (ship_row)
 
-print (ship_col)
-print (ship_row)
+    # Everything from here on should go in your for loop!
 
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
-else:
-    if guess_row == range(5) and guess_col == range(5):
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = "X"
-        print_board(board)
-    elif board[guess_row][guess_col] == "X":
-        print "You guessed that one already."
+    if guess_row == ship_row and guess_col == ship_col:
+        print ("Congratulations! You sank my battleship!")
     else:
-        print "Oops, that's not even in the ocean."
+        if guess_row == range(5) and guess_col == range(5):
+            print ("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+            print_board(board)
+        elif board[guess_row][guess_col] == "X":
+
+            print ("You guessed that one already.")
+        else:
+            print ("You missed my battleship!")
+            board[guess_row][guess_col] = "X"
+            print ("Turn", turn + 1)
+    if turn == 3:
+        print ("Game Over")
+    print_board(board)
